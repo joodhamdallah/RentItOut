befe8487c9079dfd2da0ff5c5771f756c0720f84
Leema02 <lemarizeq@gmail.com>
2024-10-29 06:44:03 +0200

    Category Management DONE

--- api.js
+++ api.js
@@ -3,12 +3,13 @@ const app = express();
 require('dotenv').config({ path: './.env' });
 const authRoutes = require('./routes/authRoutes'); // Correct path from rootconst userRoutes = require('./userRoutes');
 //const userRoutes = require('./routes/userRoutes'); // Correct path from root
+const categoryRoutes = require('./routes/categoryRoutes');
 
 
 app.use(express.json());
 app.use('/api/auth', authRoutes);  // Routes for login and signup
 //app.use('/api/users', userRoutes); // Routes for user management (requires auth)
-
+app.use('/api', categoryRoutes);
 
 const PORT = 3000;
 app.listen(PORT, () => {
